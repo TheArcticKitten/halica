aa479fbdcf9f3017397aa81c54ad182d1a8c1712
Noah Ortega <noahortega58@gmail.com>
2025-02-12 01:31:27 -0600

    added sick cat background and more tabs

--- halica/src/app/app-routing.module.ts
+++ halica/src/app/app-routing.module.ts
@@ -5,10 +5,14 @@ import { HomeComponent } from './home/home.component';
 import { CatPixComponent } from './cat-pix/cat-pix.component';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MusicComponent } from './music/music.component';
+import { ArtComponent } from './art/art.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'art', component: ArtComponent },
+  { path: 'music', component: MusicComponent },
   { path: 'cat-pix', component: CatPixComponent },
   { path: 'about', component: AboutComponent },
   { path: '**', component: PageNotFoundComponent }
